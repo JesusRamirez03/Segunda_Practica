8663fffb0f07eb91afeebabbf8769766c1033642
Jesus Ramirez <jesusal.rg.03@gmail.com>
2023-10-03 09:31:06 -0600

    primer commit

---
+++ Segunda_practica_estructura_datos/Class.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segunda_practica_estructura_datos
+{
+    internal class ArregloLetras
+    {
+        private char[] arreglo = new char[20];
+        private int nElementos = 0;
+
+        public void InicializarArreglo()
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = '\0';
+            }
+            nElementos = 0;
+            Console.WriteLine("Arreglo inicializado.");
+        }
+
+        public void MostrarArreglo()
+        {
+            Console.WriteLine("Elementos del arreglo:");
+            for (int i = 0; i < nElementos; i++)
+            {
+                Console.Write(arreglo[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Buscar()
+        {
+            Console.Write("Ingrese la letra a buscar: ");
+            char letra = char.Parse(Console.ReadLine().ToLower());
+            int indice = Array.IndexOf(arreglo, letra);
+            if (indice != -1)
+            {
+                Console.WriteLine($"'{letra}' encontrado en la posición {indice}.");
+            }
+            else
+            {
+                Console.WriteLine($"'{letra}' no encontrado en el arreglo.");
+            }
+        }
+
+        public void Insertar()
+        {
+            if (nElementos == arreglo.Length)
+            {
+                Console.WriteLine("El arreglo está lleno.");
+                return;
+            }
+
+            Console.Write("Ingrese la letra a insertar: ");
+            char letra = char.Parse(Console.ReadLine().ToLower());
+
+            int indice = 0;
+            while (indice < nElementos && arreglo[indice] < letra)
+            {
+                indice++;
+            }
+
+            for (int i = nElementos; i > indice; i--)
+            {
+                arreglo[i] = arreglo[i - 1];
+            }
+
+            arreglo[indice] = letra;
+            nElementos++;
+            Console.WriteLine($"'{letra}' insertado en la posición {indice}.");
+        }
+
+        public void Eliminar()
+        {
+            Console.Write("Ingrese la letra a eliminar: ");
+            char letra = char.Parse(Console.ReadLine().ToLower());
+
+            int indice = Array.IndexOf(arreglo, letra);
+            if (indice != -1)
+            {
+                for (int i = indice; i < nElementos - 1; i++)
+                {
+                    arreglo[i] = arreglo[i + 1];
+                }
+                arreglo[nElementos - 1] = '\0';
+                nElementos--;
+                Console.WriteLine($"'{letra}' eliminado de la posición {indice}.");
+            }
+            else
+            {
+                Console.WriteLine($"'{letra}' no encontrado en el arreglo.");
+            }
+        }
+
+        public void Modificar()
+        {
+            Console.Write("Ingrese la letra a modificar: ");
+            char letra = char.Parse(Console.ReadLine().ToLower());
+
+            int indice = Array.IndexOf(arreglo, letra);
+            if (indice != -1)
+            {
+                Console.Write("Ingrese la nueva letra: ");
+                char nuevaLetra = char.Parse(Console.ReadLine().ToLower());
+                arreglo[indice] = nuevaLetra;
+                Console.WriteLine($"'{letra}' modificado a '{nuevaLetra}' en la posición {indice}.");
+            }
+            else
+            {
+                Console.WriteLine($"'{letra}' no encontrado en el arreglo.");
+            }
+        }
+
+        public void MostrarCreditos()
+        {
+            Console.WriteLine("Créditos:");
+            Console.WriteLine("Materia: [Nombre de la materia]");
+            Console.WriteLine("Integrantes del equipo:");
+            Console.WriteLine("[Nombre del integrante 1] - [Matrícula 1]");
+            Console.WriteLine("[Nombre del integrante 2] - [Matrícula 2]");
+            // Agrega más nombres y matrículas si es necesario
+        }
+    }
+}
---
+++ Segunda_practica_estructura_datos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segunda_practica_estructura_datos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArregloLetras arregloLetras = new ArregloLetras();
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.WriteLine("Menú:");
+                Console.WriteLine("1. Inicializar / Borrar arreglo");
+                Console.WriteLine("2. Mostrar Arreglo");
+                Console.WriteLine("3. Buscar");
+                Console.WriteLine("4. Insertar");
+                Console.WriteLine("5. Eliminar");
+                Console.WriteLine("6. Modificar");
+                Console.WriteLine("7. Créditos");
+                Console.WriteLine("8. Salir");
+                Console.Write("Seleccione una opción: ");
+
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                }
+            }
+        }
+    }
+}
